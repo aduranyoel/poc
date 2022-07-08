c5566b2df9cfd097a50a9dfb8c968f5d6ade31e7
Maday Concepcion <yoet92@gmail.com>
2022-07-08 12:38:19 -0300

    add menu repository implementation

---
+++ src/ui/core/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ExampleComponent } from 'poc-core-system';
+
+function App() {
+  // TODO: load plugins
+  // TODO: render core system layout
+  return (<ExampleComponent msg="222" /> );
+}
+
+export default App;
